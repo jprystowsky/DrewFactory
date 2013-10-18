1b61f16cbbd3c581662d6b9b9bf1967d4046d352
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-10-18 10:01:01 -0400

    Oops. Lemme make that even more generic.

--- src/main/java/io/mapping/examples/DrewFactory/ParticularDeveloperFactory.java
+++ src/main/java/io/mapping/examples/DrewFactory/ParticularDeveloperFactory.java
@@ -1,5 +1,5 @@
 package io.mapping.examples.DrewFactory;
 
 public abstract class ParticularDeveloperFactory<T extends Developer> {
-	public abstract Developer getDeveloper();
+	public abstract <T extends Developer> T getDeveloper();
 }
